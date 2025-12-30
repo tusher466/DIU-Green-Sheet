88e13efa206f0b7ea88cb5c2c7ca383a2f8d5c1c
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2025-12-31 01:21:06 +0600

    Create  2748 - Output 2.c

---
+++ Input & Output/2748 - Output 2.c
@@ -0,0 +1,16 @@
+//Problem: https://judge.beecrowd.com/en/problems/view/2748
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("---------------------------------------\n");
+    printf("|        Roberto                      |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+    printf("---------------------------------------\n");
+ 
+    return 0;
+}
