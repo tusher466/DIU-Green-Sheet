42fdfc47a169a609f0420aa08c1e5534f0796371
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-01-02 03:58:52 +0600

    Create  1020 - Age in Days.c

---
+++ Simple Math/1020 - Age in Days.c
@@ -0,0 +1,15 @@
+//Problem: https://judge.beecrowd.com/en/problems/view/1020
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int years, months, days;
+    scanf("%d", &days);
+    years = days / 365;
+    months = (days % 365) / 30;
+    days = (days % 365) % 30;
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", years, months, days);
+ 
+    return 0;
+}
