f3ea338c7c557053ade959f1dc4ca81477738d30
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-01-01 20:25:44 +0600

    Create  1004 - Simple Product.c

---
+++ Simple Math/1004 - Simple Product.c
@@ -0,0 +1,13 @@
+//Problem: https://judge.beecrowd.com/en/problems/view/1004
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, PROD;
+    scanf("%d %d", &A, &B);
+    PROD = A * B;
+    printf("PROD = %d\n", PROD);
+ 
+    return 0;
+}
