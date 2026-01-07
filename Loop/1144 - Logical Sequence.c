4ac31fffa9d395a4ff93bb2c94879f37e1f850ef
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-01-07 23:28:31 +0600

    Create  1144 - Logical Sequence.c

---
+++ Loop/1144 - Logical Sequence.c
@@ -0,0 +1,15 @@
+//Prothom: https://judge.beecrowd.com/en/problems/view/1144
+
+#include <stdio.h>
+int main () {
+
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        printf("%d %d %d\n", i, i * i, i * i * i);
+        printf("%d %d %d\n", i, i * i +1, i * i * i + 1);
+        
+    }
+    
+    return 0;
+}
