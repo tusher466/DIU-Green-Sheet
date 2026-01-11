d7df230fda51eaade8d7c0a4ac91568b052e7bbd
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-01-11 23:46:17 +0600

    Create  2058 - Triangles and Regular Polygons.cpp

---
+++ Geometry/2058 - Triangles and Regular Polygons.cpp
@@ -0,0 +1,12 @@
+//Problem: https://judge.beecrowd.com/en/problems/view/2058
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long N;
+    while (cin >> N) {
+        cout << N - 2 << endl;
+    }
+    return 0;
+}
